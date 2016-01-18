5f62a694b9fddfd38c4aacd194a612016831b92f
smartdevcode <senior.dev.software@gmail.com>
2016-01-18 14:37:08 -0600

    Replaced arrow function with anonymous function to support older versions of Node.

--- src/components/FuelSavingsTextInput.js
+++ src/components/FuelSavingsTextInput.js
@@ -1,22 +1,27 @@
 import React, { Component, PropTypes } from 'react';
 
-function buildHandleChange(props) {
-  return function handleChange(e) {
-    props.onChange(props.name, e.target.value);
-  };
+class FuelSavingsTextInput extends Component {
+	constructor(props, context) {
+		super(props, context);
+
+		//why: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(e) {
+		this.props.onChange(this.props.name, e.target.value);
+	}
+
+	render() {
+		return (
+			<input className="small"
+				type="text"
+				placeholder={this.props.placeholder}
+				value={this.props.value}
+				onChange={this.handleChange} />
+		);
+	}
 }
-
-const FuelSavingsTextInput = (props) => {
-  const handleChange = buildHandleChange(props);
-
-  return (
-    <input className="small"
-      type="text"
-      placeholder={props.placeholder}
-      value={props.value}
-      onChange={handleChange} />
-	);
-};
 
 FuelSavingsTextInput.propTypes = {
 	name: PropTypes.string.isRequired,
