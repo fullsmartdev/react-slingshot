ccd9632c24813b6c45a2925d01daac801d03d8af
smartdevcode <senior.dev.software@gmail.com>
2016-05-13 11:36:49 +0200

    Fixed some spec files being ignored

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -11,9 +11,15 @@ process.env.NODE_ENV = 'production';
 
 // Disable webpack-specific features for tests since
 // Mocha doesn't know what to do with them.
-['.css', '.scss', '.png', '.jpg'].forEach(ext => {
-  require.extensions[ext] = () => null;
-});
+require.extensions['.css'] = () => {
+  return null;
+};
+require.extensions['.png'] = () => {
+  return null;
+};
+require.extensions['.jpg'] = () => {
+  return null;
+};
 
 // Register babel so that it will transpile ES6 to ES5
 // before our tests run.
